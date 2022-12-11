76cdfdf026e867352d9aafc04ba30562d89ba2ce
Terje Urnes <terje@urnes.net>
2022-12-11 14:36:49 +0100

    hello from lambda

--- lambda.py
+++ lambda.py
@@ -1,8 +1,14 @@
 import json
 
+HEADERS = {
+    'Access-Control-Allow-Origin': 'https://tictac.t79.app',
+    'Access-Control-Allow-Headers': '*'
+}
+
 def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
+        'headers': HEADERS,
         'body': json.dumps('Hello from Lambda!')
     }
